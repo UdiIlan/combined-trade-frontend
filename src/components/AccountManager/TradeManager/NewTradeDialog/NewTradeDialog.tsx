d971f0248fd73025b730ba16adcdf19978a98a2e
izikorgad <izik.orgad@bitmain.com>
2018-11-21 15:04:14 +0200

    TradeManager component

---
+++ src/components/AccountManager/TradeManager/NewTradeDialog/NewTradeDialog.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+const styles = require('./styles.scss');
+
+export default class NewTradeDialog extends React.Component<any, any> {
+  render() {
+    return (
+      <div className={styles.newTradeDialog}>
+
+      </div>
+    );
+  }
+}
