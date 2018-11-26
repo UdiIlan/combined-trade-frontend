acd9377cf5bb0bc3b9ecd4b80772a326babfdc8c
Shirley Marchand <shirley.marchand@bitmain.com>
2018-11-26 15:26:10 +0200

    add trades preview

--- src/components/AccountManager/AccountDashboard/AccountDashboard.tsx
+++ src/components/AccountManager/AccountDashboard/AccountDashboard.tsx
@@ -11,7 +11,7 @@ const styles = require('./styles.scss');
 const classNames = require('classnames/bind');
 const cx = classNames.bind(styles);
 
-interface AccountDashboardProps {
+export interface AccountDashboardProps {
     account: Account;
     accountBalance?: object;
     accountTrades?: object;
@@ -20,7 +20,7 @@ interface AccountDashboardProps {
 }
 
 
-export default class AccountDashboard extends React.Component<AccountDashboardProps, any> {
+class AccountDashboard extends React.Component<AccountDashboardProps, any> {
 
     constructor(props) {
         super(props);
@@ -35,13 +35,16 @@ export default class AccountDashboard extends React.Component<AccountDashboardPr
             <div className={styles.dashboard}>
 
                 <div className={styles.dashboardContent}>
-                    <InputText className={styles.description} disabled= {true} outlined label='description' value={this.props.account ? this.props.account.description : 'default account description'}> </InputText>
+                    <InputText className={styles.description} disabled={true} outlined label='description' value={this.props.account ? this.props.account.description : 'default account description'}> </InputText>
                     <div className={styles.widgetColumn} >
 
-                        <Widget title={'Balance'} className={styles.widget} loading={!this.props.accountBalance}>
+                        <Widget title={<div className={styles.title}>Trades</div>} className={styles.widget} loading={!this.props.accountBalance}>
                         </Widget>
 
-                        <Widget title={<div className={styles.title}>Trades<Link className={styles.link} to={'/trades'} /></div>} className={styles.middleWidget} loading={!this.props.accountTrades}>
+                        <Widget title={<div className={styles.title}>Trades<Link className={styles.link} to={'/trades'} /></div>} className={styles.middleWidget} /*loading={!this.props.accountTrades}*/>
+                            <div>
+                                trades
+                            </div>
                         </Widget>
 
                         <Widget title={<div className={styles.title}>Funds<Link className={styles.link} to={'/funds'} /></div>} className={styles.widget} loading={!this.props.accountFunds}>
@@ -54,3 +57,21 @@ export default class AccountDashboard extends React.Component<AccountDashboardPr
         );
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        accounts: _.get(state, 'account.accounts'),
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    // return {
+    //   getAccounts: () => dispatch(getAccounts()),
+    //   createNewAccount: (name, description) => dispatch(createAccount({ name, description })),
+    //   editAccount: (name, description) => dispatch(editAccount({ name, description })),
+    //   deleteAccount: (name) => dispatch(deleteAccount(name)),
+    //   getTrades: (account: Account) => dispatch(fetchAccountTrades(account))
+    // };
+};
+
+export default connect(mapStateToProps, {})(AccountDashboard);
